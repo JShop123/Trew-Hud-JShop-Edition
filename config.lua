0e4219205f69aae7ca39adb2871a244d5232246d
JShop <156724160+JShop123@users.noreply.github.com>
2025-02-17 16:17:22 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,64 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.serverLogo = 'img/logo.png'
+
+Config.font = {
+	name 	= 'Signika Negative',
+	url 	= 'https://fonts.googleapis.com/css2?family=Signika+Negative&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= true
+}
+
+Config.vehicle = {
+	speedUnit = 'MPH',
+	maxSpeed = 180,
+
+	keys = {
+		seatbelt 	= 'K',
+		cruiser		= 'CAPS',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'DOWN',
+	}
+}
+
+Config.ui = {
+	showServerLogo		 = true,
+
+	showJob		 		 = true,
+
+	showWalletMoney 	 = true,
+	showBankMoney 		 = true,
+	showBlackMoney 		 = true,
+	showSocietyMoney	 = true,
+	showVipMoney         = true,
+
+	showDate 			 = true,
+	showLocation 		 = true,
+	showVoice	 		 = true,
+
+	showHealth			 = true,
+	showArmor	 		 = true,
+	showStamina	 		 = true,
+	showHunger 			 = true,
+	showThirst	 		 = true,
+	showStress			 = true,
+	showDrunk		 	 = true,
+	showPoo		 	 	 = true,
+	showPee		 	 	 = true,
+	showBath		     = true,
+	showGym		     	 = true,
+	showOxygen			 = true,
+	showID	 			 = true,
+
+	showMinimapOnFoot 	 = false,
+	showMinimapInVehicle = true,
+	showMinimapHealth    = false,
+
+	showWeapons			 = true,	
+}
